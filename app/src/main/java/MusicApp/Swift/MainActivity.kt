7dd011a0c7c03d52af19341dcf9922cee6ff1344
veer <sjaiveer024@gmail.com>
2023-04-27 08:01:20 +0530

    INITIAL COMMIT

---
+++ app/src/main/java/MusicApp/Swift/MainActivity.kt
@@ -0,0 +1,11 @@
+package MusicApp.Swift
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
